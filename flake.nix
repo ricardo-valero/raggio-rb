6732c1ec8cf7297b20479e87d6f687f1604139fb
Ricardo Valero de la Rosa <55701657+ricardo-valero@users.noreply.github.com>
2025-10-16 16:52:27 -0600

    initial setup

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    systems = nixpkgs.lib.systems.flakeExposed;
+  in {
+    devShells = nixpkgs.lib.genAttrs systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = builtins.attrValues {
+          inherit (pkgs) git nixd alejandra;
+          ruby = pkgs.ruby.withPackages (r: with r; [ruby-lsp]);
+        };
+      };
+    });
+  };
+}
